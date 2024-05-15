0cc32aec3e7892038ab8ade4bc83e8aa72cd2d0e
daniel98 <daniel.andres.caicedo@correounivalle.edu.co>
2024-05-14 22:14:23 -0500

    mejorando laplaciano

--- laplacian_seg.py
+++ laplacian_seg.py
@@ -1,51 +1,23 @@
 import numpy as np
 import math
-from scipy.optimize import minimize
-import nibabel as nib
-from tkinter import filedialog
 
-img = nib.load('imagen_recortada_2.nii')
-# img_data = img.get_fdata()
- 
-# B = [
-#     (4, 0, 6), (4, 0, 6), (4, 0, 6), (4, 0, 6), (4, 0, 6), (4, 0, 6), (4, 0, 6), (4, 0, 6), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 5), (4, 0, 4), (4, 0, 4), (4, 0, 4), (4, 0, 4), (4, 0, 4), (4, 0, 4), (4, 0, 4), (4, 0, 4), (4, 0, 4), (4, 0, 4), (4, 0, 4), (4, 0, 4), (4, 0, 4), (4, 0, 4), (4, 0, 4), (4, 0, 4), (4, 0, 4), (4, 0, 4), (4, 1, 4), (4, 1, 4), (4, 1, 4), (4, 1, 4), (4, 1, 4), (4, 1, 4), (4, 1, 4), (4, 1, 4), (4, 1, 4), (4, 1, 4), (4, 1, 4), (4, 1, 4), (4, 1, 4), (4, 1, 4), (4, 1, 4), (4, 1, 4), (4, 1, 3), (4, 1, 3), (4, 1, 3), (4, 1, 3), (4, 2, 3), (4, 2, 3), (4, 2, 3), (4, 2, 3), (4, 2, 3), (4, 2, 3), (4, 2, 3), (4, 2, 3), (4, 2, 3), (4, 2, 3), (4, 2, 3), (4, 2, 3), (4, 2, 3), (4, 2, 3), (4, 2, 3), (4, 2, 3), (4, 3, 3), (4, 3, 3), (4, 3, 3), (4, 3, 3), (4, 3, 3), (4, 3, 3), (4, 3, 3), (4, 3, 3), (4, 3, 3), (4, 3, 3), (4, 3, 3), (4, 3, 3), (4, 3, 3), (4, 3, 3), (4, 3, 3), (4, 4, 3), (4, 4, 3), (4, 4, 3), (4, 4, 3), (4, 4, 3), (4, 4, 3), (4, 4, 3), (4, 4, 3), (4, 4, 3), (4, 4, 4), (4, 4, 4), (4, 4, 4), (4, 4, 4), (4, 4, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 4), (4, 5, 5), (4, 5, 5), (4, 5, 5), (4, 5, 5), (4, 5, 5), (4, 5, 5), (4, 5, 5), (4, 5, 5), (4, 5, 5), (4, 5, 5), (4, 5, 5), (4, 5, 5), (4, 5, 5), (4, 5, 5), (4, 5, 5), (4, 5, 5), (4, 5, 6), (4, 5, 6), (4, 5, 6), (4, 5, 6), (4, 5, 6), (4, 5, 6), (4, 5, 6), (4, 5, 6), (4, 5, 6), (4, 5, 6), (4, 5, 6), (4, 5, 6), (4, 5, 6), (4, 5, 6), (4, 5, 6), (4, 6, 6), (4, 6, 6), (4, 6, 7), (4, 6, 7), (4, 6, 7), (4, 6, 7), (4, 6, 7), (4, 6, 7), (4, 6, 7), (4, 6, 7), (4, 6, 7), (4, 6, 7), (4, 6, 7), (4, 6, 7), (4, 0, 6), (4, 0, 6), (4, 0, 6), (4, 0, 6), (4, 0, 6), (4, 0, 6), (4, 0, 6), (4, 0, 6), (4, 0, 6)]
 
-# F = [
-#     (4, 0, 3), (4, 0, 3), (4, 0, 3), (4, 0, 3), (4, 0, 3), (4, 0, 3), (4, 0, 3), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 2), (4, 0, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 1, 1), (4, 2, 1), (4, 2, 1), (4, 2, 1), (4, 2, 1), (4, 2, 1), (4, 2, 1), (4, 2, 1), (4, 2, 1), (4, 2, 1), (4, 2, 1), (4, 3, 1), (4, 3, 1), (4, 3, 1), (4, 3, 1), (4, 3, 1), (4, 3, 1), (4, 3, 1), (4, 3, 1), (4, 3, 1), (4, 3, 1), (4, 3, 1), (4, 3, 1), (4, 3, 1), (4, 3, 1), (4, 4, 1), (4, 4, 1), (4, 4, 1), (4, 4, 1), (4, 4, 1), (4, 4, 1), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 4, 2), (4, 5, 2), (4, 5, 2), (4, 5, 2), (4, 5, 2), (4, 5, 2), (4, 5, 2), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 5, 3), (4, 6, 3), (4, 6, 3), (4, 6, 3), (4, 6, 3), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 4), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 6, 5), (4, 7, 5), (4, 7, 5), (4, 7, 5), (4, 7, 5), (4, 7, 5), (4, 7, 5), (4, 7, 5), (4, 7, 6), (4, 7, 6), (4, 7, 6), (4, 7, 6), (4, 7, 6), (4, 7, 6), (4, 7, 6), (4, 7, 6), (4, 7, 6), (4, 7, 6)]
 
-# B = list(set(B))
-# F = list(set(F))
-img_data= np.array([[[10, 10, 10, 10],
-                          [10, 20, 20, 10],
-                          [10, 20, 20, 10],
-                          [10, 10, 10, 10]],
-                        
-                         [[335, 5, 5, 5],
-                          [5, 15, 15, 5],
-                          [5, 15, 15, 5],
-                          [5, 5, 5, 5]],
 
-                         [[10, 10, 10, 10],
-                          [10, 20, 20, 10],
-                          [10, 20, 20, 10],
-                          [10, 10, 10, 10]],
+img_data= np.array(
+    [[ 0,  1.,  2. , 3.],
+    [ 4.,  200. , 30,  7.],
+    [ 200. , 9., 10., 21.]])
 
-                        [[5, 5, 5, 5],
-                          [5, 15, 15, 5],
-                          [5, 15, 15, 5],
-                          [5, 5, 5, 5]],
-                          ])
-
-F = [(0, 0, 0), (0, 0, 1), (0, 0, 2), (0, 0, 3), (0, 1, 0), (0, 1, 1), (0, 1, 2), (0, 1, 3)]
-B = [(2, 3, 3), (3, 0, 0), (3, 0, 1), (3, 0, 2), (3, 0, 3), (3, 1, 0), (3, 1, 1), (3, 1, 2),
-    (3, 1, 3), (3, 2, 0), (3, 2, 1), (3, 2, 2)]
-height, width, depth, = img_data.shape
+F = [(0, 0), (0, 1)]
+B = [(2, 1), (2, 0), (2, 2)]
+height, width = img_data.shape
 
 B = list(set(B))  
 F = list(set(F))  
-V = [(d, h, w) for d in range(height) for h in range(width) for w in range(depth)]
+V = [(d, h) for d in range(height) for h in range(width)]
 E = []
-W = []
+W_e = []
 N = []
 D = []
 # Crear una matriz para la nueva imagen con la misma forma que la imagen original
@@ -73,99 +45,114 @@ def calculate_wijk(edges,sigma):
     # print(w)   
     return w
 cont = 0
+
 for i in range(height):    
     for j in range(width):
-        for k in range(depth):
-            # Obtener el valor del voxel actual     
-            cont += 1       
-            voxel_actual = img_data[i, j, k]
-            # Obtener los valores de los 6 vecinos
-            vecinos = [
-                        img_data[min(i+1, height-1), j, k], img_data[max(i-1, 0), j, k],
-                        img_data[i, min(j+1, width-1), k], img_data[i, max(j-1, 0), k],
-                        img_data[i, j, min(k+1, depth-1)], img_data[i, j, max(k-1, 0)]
-                    ]
-            e = [
-                (voxel_actual,vecinos[0]) , (voxel_actual,vecinos[1]),
-                (voxel_actual,vecinos[2]) , (voxel_actual,vecinos[3]),
-                (voxel_actual,vecinos[4]) , (voxel_actual,vecinos[5])
-            ]
+        # Obtener el valor del píxel actual
+        voxel_actual = img_data[i, j]
+        
+        # Obtener los valores de los 8 vecinos
+        vecinos = [
+            img_data[max(i-1, 0), max(j-1, 0)], img_data[max(i-1, 0), j],
+            img_data[max(i-1, 0), min(j+1, width-1)], img_data[i, max(j-1, 0)],
+            img_data[i, min(j+1, width-1)], img_data[min(i+1, height-1), max(j-1, 0)],
+            img_data[min(i+1, height-1), j], img_data[min(i+1, height-1), min(j+1, width-1)]
+        ]
+        
+        # Crear las aristas y los índices de los vecinos
+        e = [
+            (voxel_actual, vecinos[0]), (voxel_actual, vecinos[1]),
+            (voxel_actual, vecinos[2]), (voxel_actual, vecinos[3]),
+            (voxel_actual, vecinos[4]), (voxel_actual, vecinos[5]),
+            (voxel_actual, vecinos[6]), (voxel_actual, vecinos[7])
+        ]
+        print(e)
 
-            n = [
-                V.index((min(i+1, height-1), j, k)), V.index((max(i-1, 0), j, k)),
-                V.index((i, min(j+1, width-1), k)), V.index((i, max(j-1, 0), k)),
-                V.index((i, j, min(k+1, depth-1))), V.index((i, j, max(k-1, 0)))
-            ]
-            N.append(n)
-            E.append(e)
-            sigma = (10**-6) + calculate_sigma(e)
-            w = calculate_wijk(e,sigma)
-            d = sum(w)
-            D.append(d)
-            W.append(w)
+        n = [
+            V.index((max(i-1, 0), max(j-1, 0))), V.index((max(i-1, 0), j)),
+            V.index((max(i-1, 0), min(j+1, width-1))), V.index((i, max(j-1, 0))),
+            V.index((i, min(j+1, width-1))), V.index((min(i+1, height-1), max(j-1, 0))),
+            V.index((min(i+1, height-1), j)), V.index((min(i+1, height-1), min(j+1, width-1)))
+        ]
 
-k1 = 1
-k2 = 1
-k3 = 1
+        indice_actual = i * width + j
+        
+        # Obtener los índices de los 8 vecinos
+        nbh = [
+            max(i-1, 0) * width + max(j-1, 0), max(i-1, 0) * width + j,
+            max(i-1, 0) * width + min(j+1, width-1), i * width + max(j-1, 0),
+            i * width + min(j+1, width-1), min(i+1, height-1) * width + max(j-1, 0),
+            min(i+1, height-1) * width + j, min(i+1, height-1) * width + min(j+1, width-1)
+        ]
+        
+        # Crear las aristas
+        nb = [(indice_actual, nb) for nb in nbh]
+        
+        # Agregar las aristas al arreglo E
+        # E.append(nb)
+
+        N.append(n)
+        E.append(nb)
+        sigma = (10**-6) + calculate_sigma(e)
+        w = calculate_wijk(e,sigma)
+        d = sum(w)
+        D.append(d)
+        W_e.append(w)
+
+
+
+D_m = np.diag(np.sum(W_e, axis=1))
+
+
+W = np.zeros((height * width, height * width))
+for i in range(height * width):    
+    for j in range(height * width):
+        # Calcular la matriz de adyacencia ponderada W
+        index = (i, j)
+        for e in E:
+            if index in e: 
+                W[i, j] = W_e[i][e.index(index)]
+
+# Imprimir la matriz de adyacencia ponderada W
+print("Matriz de adyacencia ponderada W:")
+
+S = B + F  # Concatenar las listas B y F
+S_indices = [V.index(pixel) for pixel in S if pixel in V]  # Obtener los índices de los elementos en S que están en V
+
+I_s = np.zeros((height * width, height * width))  # Inicializar matriz diagonal de ceros
+np.fill_diagonal(I_s, 1, list(S_indices))  # Establecer los elementos correspondientes a 1
 
 xB = 1
 xF = 0.1
 
+L = D_m - W
+
+b = np.zeros((height * width, 1))
+for pixel in V:
+    if pixel in B:        
+        b[V.index(pixel)] = xB
+    if pixel in F:        
+        b[V.index(pixel)] = xF    
 
 
-def loss_function(x):
-    suma_b = 0
-    suma_f = 0
-    suma_v = 0
-    Ex = 0
-    for i in range(len(V)):   
-        if  V[i] in B:
-            suma_b += k1 * np.linalg.norm(x[i] - xB)**2 
-        if  V[i] in F:    
-            suma_f = k2 * np.linalg.norm(x[i] - xF)**2
-        if V[i] in V:    
-            suma_v = k3 * np.linalg.norm(x[i]*D[i] - sum(W[i][j]*x[j] for j in range(0,len(N[i]))) )**2    
-            # suma_v = k3 * np.linalg.norm(x[i]*D[i] - sum(W[i][N[i][j]]*x[j] for j in range(0,len(N[i]))) )**2            
-                    
-    Ex += suma_b + suma_f + suma_v
-    return Ex   
 
-x_initial = np.zeros(len(V))
+print("Vector b:")
+# print(V)
+# print(b)
 
-# Minimización de la función de pérdida
-# result = minimize(loss_function, x_initial, method='CG')
-
-result = minimize(loss_function, x_initial,method='BFGS', tol=1e-3)
-
-# Los valores óptimos de x
-x_optimal = result.x
-# print(len(x_optimal))
-# print("Valores óptimos de x que minimizan En:")
-# print(x_optimal)
-y = np.zeros(len(V))
-
-
-for i in range(len(V)):
-    if (x_optimal[i] > ((xB + xF) / 2)):
-        
+x = np.linalg.solve(I_s + L**2, b)
+y = np.zeros_like(x)
+for i in range(len(x)):
+    if x[i] >= (xB + xF) / 2:
         y[i] = xB
     else:
-        
-        y[i] = xF    
-
-# file_path = filedialog.asksaveasfilename(defaultextension=".nii", filetypes=[("NIFTI files", "*.nii")])
-# if file_path:
-#     # Crear un objeto Nibabel con los datos de la segmentación
-#     img_nifti = nib.Nifti1Image(nueva_img_data, img.affine)
-#     nib.save(img_nifti, file_path)
-#     print(f"Segmentación guardada como '{file_path}'")
+        y[i] = xF
 
 
+print("Solución x:")
 
-
-# new_img = y.reshape(img_data.shape)
-# imagen_nueva = nib.Nifti1Image(new_img, np.eye(4))
-# nib.save(imagen_nueva, 'imagen_deg_lap.nii')
-
-# print(E[0])
-print(W[11])
+nueva_img_data = y.reshape(img_data.shape)
+print(nueva_img_data)
+print(x)
+print(y)
+# print(L)
--- segmentacion/laplacian_segmentation.py
+++ segmentacion/laplacian_segmentation.py
@@ -4,44 +4,20 @@ from scipy.optimize import minimize
 import nibabel as nib
 from tkinter import filedialog
 
-# img = nib.load('imagen.nii')
-def laplacian_coordinates_segmentation(img,B,F):
-    img_data = img.get_fdata()
-    # img_data= np.array([[[10, 10, 10, 10],
-    #                       [10, 20, 20, 10],
-    #                       [10, 20, 20, 10],
-    #                       [10, 10, 10, 10]],
-                        
-    #                      [[335, 5, 5, 5],
-    #                       [5, 15, 15, 5],
-    #                       [5, 15, 15, 5],
-    #                       [5, 5, 5, 5]],
+def laplacian_coordinates_segmentation(img, B, F):
+    img_data = img.get_fdata()    
+    height, width , p = img_data.shape
 
-    #                      [[10, 10, 10, 10],
-    #                       [10, 20, 20, 10],
-    #                       [10, 20, 20, 10],
-    #                       [10, 10, 10, 10]],
-
-    #                     [[5, 5, 5, 5],
-    #                       [5, 15, 15, 5],
-    #                       [5, 15, 15, 5],
-    #                       [5, 5, 5, 5]],
-    #                       ])
-
-    # B = [(0, 0, 0), (0, 0, 1), (0, 0, 2), (0, 0, 3), (0, 1, 0), (0, 1, 1), (0, 1, 2), (0, 1, 3)]
-    # F = [(2, 3, 3), (3, 0, 0), (3, 0, 1), (3, 0, 2), (3, 0, 3), (3, 1, 0), (3, 1, 1), (3, 1, 2),
-    #     (3, 1, 3), (3, 2, 0), (3, 2, 1), (3, 2, 2)]    
-
-    height, width, depth, = img_data.shape
-
-    # Definiendo las variables necesarias
-    V = [(d, h, w) for d in range(height) for h in range(width) for w in range(depth)]
+    B = np.array([list(pixel[:-1]) for pixel in B])
+    F = np.array([list(pixel[:-1]) for pixel in F])
+    V = [(d, h) for d in range(height) for h in range(width)]
     E = []
-    W = []
+    W_e = []
     N = []
     D = []
-           
-    #Calcular sigma       
+    
+    nueva_img_data = np.zeros_like(img_data)
+
     def calculate_sigma(edges):
         sigma = 0
         for i in edges:
@@ -51,99 +27,94 @@ def laplacian_coordinates_segmentation(img,B,F):
                 sigma = max_diff
         return sigma
 
-    def calculate_wijk(edges,sigma):    
+    def calculate_wijk(edges, sigma):    
         w = []
+        β = 0.1
         for i in edges:
             diff = np.abs(i[0] - i[1])      
-            max_diff = np.max(diff)**2
-            betha = 0.1 * max_diff
-            div = -1 * (betha / sigma)
+            max_diff = β * np.max(diff)**2        
+            div = -1 * (max_diff / sigma)
             exp = math.exp(div)  
             w.append(exp)
-        # print(w)   
         return w
-    cont = 0
+    
     for i in range(height):    
         for j in range(width):
-            for k in range(depth):
-                # Obtener el valor del voxel actual     
-                cont += 1       
-                voxel_actual = img_data[i, j, k]
-                # Obtener los valores de los 6 vecinos
-                vecinos = [
-                            img_data[min(i+1, height-1), j, k], img_data[max(i-1, 0), j, k],
-                            img_data[i, min(j+1, width-1), k], img_data[i, max(j-1, 0), k],
-                            img_data[i, j, min(k+1, depth-1)], img_data[i, j, max(k-1, 0)]
-                        ]
-                e = [
-                    (voxel_actual,vecinos[0]) , (voxel_actual,vecinos[1]),
-                    (voxel_actual,vecinos[2]) , (voxel_actual,vecinos[3]),
-                    (voxel_actual,vecinos[4]) , (voxel_actual,vecinos[5])
-                ]
+            voxel_actual = img_data[i, j]
+            
+            vecinos = [
+                img_data[max(i-1, 0), max(j-1, 0)], img_data[max(i-1, 0), j],
+                img_data[max(i-1, 0), min(j+1, width-1)], img_data[i, max(j-1, 0)],
+                img_data[i, min(j+1, width-1)], img_data[min(i+1, height-1), max(j-1, 0)],
+                img_data[min(i+1, height-1), j], img_data[min(i+1, height-1), min(j+1, width-1)]
+            ]
+            
+            e = [
+                (voxel_actual, vecinos[0]), (voxel_actual, vecinos[1]),
+                (voxel_actual, vecinos[2]), (voxel_actual, vecinos[3]),
+                (voxel_actual, vecinos[4]), (voxel_actual, vecinos[5]),
+                (voxel_actual, vecinos[6]), (voxel_actual, vecinos[7])
+            ]
 
-                n = [
-                    V.index((min(i+1, height-1), j, k)), V.index((max(i-1, 0), j, k)),
-                    V.index((i, min(j+1, width-1), k)), V.index((i, max(j-1, 0), k)),
-                    V.index((i, j, min(k+1, depth-1))), V.index((i, j, max(k-1, 0)))
-                ]
-                N.append(n)
-                E.append(e)
-                sigma = (10**-6) + calculate_sigma(e)
-                w = calculate_wijk(e,sigma)
-                d = sum(w)
-                D.append(d)
-                W.append(w)
-    
-    k1 = 1
-    k2 = 1
-    k3 = 1
+            n = [
+                V.index((max(i-1, 0), max(j-1, 0))), V.index((max(i-1, 0), j)),
+                V.index((max(i-1, 0), min(j+1, width-1))), V.index((i, max(j-1, 0))),
+                V.index((i, min(j+1, width-1))), V.index((min(i+1, height-1), max(j-1, 0))),
+                V.index((min(i+1, height-1), j)), V.index((min(i+1, height-1), min(j+1, width-1)))
+            ]
+
+            indice_actual = i * width + j
+            
+            nbh = [
+                max(i-1, 0) * width + max(j-1, 0), max(i-1, 0) * width + j,
+                max(i-1, 0) * width + min(j+1, width-1), i * width + max(j-1, 0),
+                i * width + min(j+1, width-1), min(i+1, height-1) * width + max(j-1, 0),
+                min(i+1, height-1) * width + j, min(i+1, height-1) * width + min(j+1, width-1)
+            ]
+            
+            nb = [(indice_actual, nb) for nb in nbh]
+            
+            N.append(n)
+            E.append(nb)
+            sigma = (10**-6) + calculate_sigma(e)
+            w = calculate_wijk(e, sigma)
+            d = sum(w)
+            D.append(d)
+            W_e.append(w)
+
+    D_m = np.diag(np.sum(W_e, axis=1))
+
+    S = np.concatenate((B,F))
+    # S_indices = [V.index(pixel) for pixel in S if pixel in V]  
+    S_indices = np.where(np.isin(V, S).all(axis=1))[0]
+
+    I_s = np.zeros((height * width, height * width))  
+    np.fill_diagonal(I_s, 1, list(S_indices))  
 
     xB = 1
     xF = 0.1
-    
-    
 
-    def loss_function(x):
-        suma_b = 0
-        suma_f = 0
-        suma_v = 0
-        Ex = 0
-        for i in range(len(V)):   
-            if  V[i] in B:
-                suma_b += k1 * np.linalg.norm(x[i] - xB)**2 
-            if  V[i] in F:    
-                suma_f = k2 * np.linalg.norm(x[i] - xF)**2
-            if V[i] in V:    
-                suma_v = k3 * np.linalg.norm(D[i]*x[i] - sum(W[i][j]*x[j] for j in range(len(N[i]))))**2            
-        Ex += suma_b + suma_f + suma_v
-        return Ex   
+    L = D_m - np.array(W_e)
 
-    x_initial = np.zeros(len(V))
+    b = np.zeros((height * width, 1))
+    for pixel in V:
+        if pixel in B:        
+            b[V.index(pixel)] = xB
+        if pixel in F:        
+            b[V.index(pixel)] = xF    
 
-    # Minimización de la función de pérdida
-    # result = minimize(loss_function, x_initial, method='CG')
-    result = minimize(loss_function, x_initial,method='BFGS', tol=1e-3)
-
-    # Los valores óptimos de x
-    x_optimal = result.x
-    
-    y = np.zeros(len(V))
-    # nueva_img_data = np.zeros_like(img_data)
-
-
-    for i in range(len(V)):
-        if (x_optimal[i] > ((xB + xF) / 2)):
-            # nueva_img_data[V[i]] = xB
+    x = np.linalg.solve(I_s + L**2, b)
+    y = np.zeros_like(x)
+    for i in range(len(x)):
+        if x[i] >= (xB + xF) / 2:
             y[i] = xB
         else:
-            # nueva_img_data[V[i]] = xF
-            y[i] = xF    
+            y[i] = xF
+
     nueva_img_data = y.reshape(img_data.shape)
     file_path = filedialog.asksaveasfilename(defaultextension=".nii", filetypes=[("NIFTI files", "*.nii")])
     if file_path:
-        # Crear un objeto Nibabel con los datos de la segmentación
         img_nifti = nib.Nifti1Image(nueva_img_data, img.affine)
         nib.save(img_nifti, file_path)
         print(f"Segmentación guardada como '{file_path}'")
-
-    print(e)    
+       
